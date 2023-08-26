d414e18bc0ddf8c9b9dc81a2c7ccac185bd74222
손성준 <sonsj97@gmail.com>
2023-08-26 23:48:25 +0900

    memo 제거

--- src/components/ChatScreen.js
+++ src/components/ChatScreen.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, memo } from 'react';
+import React, { useRef, useEffect } from 'react';
 import MessageCard from './MessageCard';
 import ChatButton from './ChatButton';
 
